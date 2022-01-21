1af54e23a2329e58f7372d320f67450c2abebe42
kylezapalac <kyle.zapalac@gmail.com>
2022-01-21 10:52:59 -0600

    FirstCommit

---
+++ Wk1_HelloWorld_KZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wk1_HelloWorld_KZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
